8ee1b69c421bca48e9236119b401a73e1167ae74
MayurTA <50455012+MayurTA@users.noreply.github.com>
2021-01-14 17:31:08 +0530

    Add files via upload
    These files include final.mag file - describing the layout of the final 4-bit ALU circuit, which can be opened and edited in MAGIC, final.ext - the extracted file of the layout, final.sim - used in simulating the circuit using IRSIM, final.cmd - a script to run the sequence of commands to test the circuit in IRSIM

---
+++ final.cmd
@@ -0,0 +1,46 @@
+h vdd
+l vss
+analyzer a3 a2 a1 a0 b3 b2 b1 b0 d3 d2 d1 d0 lt gt eq m3 m2 m1 m0 n3 n2 n1 n0 s3 s2 s1 s0 cin cs3 cnt p3 p2 p1 p0 sh3 sh2 sh1 sh0
+w a0 a1 a2 a3 b0 b1 b2 b3 d0 d1 d2 d3 lt gt eq m3 m2 m1 m0 n3 n2 n1 n0 s3 s2 s1 s0 cin cs3 cnt p3 p2 p1 p0 sh3 sh2 sh1 sh0
+vector a a3 a2 a1 a0
+vector b b3 b2 b1 b0
+vector m m3 m2 m1 m0
+vector n n3 n2 n1 n0
+vector in p3 p2 p1 p0
+setvector a 0000
+setvector b 0000
+h c
+setvector m 1111
+setvector n 1111
+h cin
+setvector in 1011
+l cnt
+s
+setvector a 1111
+setvector b 1111
+h c
+setvector m 0000
+setvector n 0000
+h cin
+setvector in 1111
+h cnt
+s
+setvector a 1111
+setvector b 0000
+h c
+setvector m 0000
+setvector n 1111
+l cin
+setvector in 1101
+h cnt
+s
+setvector a 0000
+setvector b 1111
+h c
+setvector m 1111
+setvector n 0000
+l cin
+setvector in 1110
+l cnt
+s
+
